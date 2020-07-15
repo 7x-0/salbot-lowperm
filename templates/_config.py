e656b79314c6d03750b7740cf629e1affcd4e984
vcokltfre <vcokltfre@pop-os.localdomain>
2020-07-15 10:45:15 +0100

    chore(docs): add config file template

---
+++ templates/_config.py
@@ -0,0 +1,10 @@
+"""
+Created by vcokltfre - 2020-07-15
+"""
+
+TOKEN = "your discord token" # Your token, obtained from https://discord.com/developers
+
+MOD_ROLES = ["Administrator", "Moderator"]
+
+BOT_SPAM_ONLY = True # Whether the bot should only allow commands in a bot spam channel to non exempt roles
+BOT_SPAM_CHANNELS = [1234567890, 2345678910]
