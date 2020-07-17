6dc4d3b116ec74cb518c0ae312a0abfe4ff09b5f
vcokltfre <vcokltfre@pop-os.localdomain>
2020-07-18 00:24:36 +0100

    fix: remove badges by creator request

--- templates/_config.py
+++ templates/_config.py
@@ -8,7 +8,3 @@ MOD_ROLES = ["Administrator", "Moderator"]
 
 BOT_SPAM_ONLY = True # Whether the bot should only allow commands in a bot spam channel to non exempt roles
 BOT_SPAM_CHANNELS = [1234567890, 2345678910]
-
-BADGES = {
-    "contributor": "<:contributor:733427564420530178>"
-}
