31711e3eb696afe96b00bc9184af7b01604e366c
vcokltfre <vcokltfre@pop-os.localdomain>
2020-07-17 15:31:54 +0100

    fix: make config have return trypes

--- helpers/config.py
+++ helpers/config.py
@@ -1,12 +1,15 @@
 import json
 from pathlib import Path
 
-def make_data():
+def make_data() -> bool:
     if not Path.exists("./data"):
         Path.mkdir("./data")
+        return True
+    return False
 
-def make_cfg(name: str, initial_data: object = []):
+def make_cfg(name: str, initial_data: object = []) -> bool:
     if not Path.exists(f"./data/{name}.json"):
         with open(f"./data/{name}.json", 'w') as f:
             json.dump(initial_data, f)
-
+        return True
+    return False
