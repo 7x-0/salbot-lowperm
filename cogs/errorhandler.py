04bcd7e1275185c7ca40f5f15f4406e55ace772b
vcokltfre <vcokltfre@pop-os.localdomain>
2020-07-19 12:10:20 +0100

    feat: add pause before deleting error trigger

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -4,6 +4,7 @@ Created by Epic - 2020-07-17
 
 import discord
 from discord.ext import commands
+import asyncio
 
 
 class ErrorHandler(commands.Cog):
@@ -15,8 +16,9 @@ class ErrorHandler(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error):
         if isinstance(error, commands.CheckFailure):
+            await ctx.send(str(error), delete_after=15)
+            await asyncio.sleep(5)
             await ctx.message.delete()
-            await ctx.send(str(error), delete_after=30)
         else:
             await ctx.send("Oh no! Something went wrong, please us `%help` for help.", delete_after=30)
 
