c9596389bb9e188e19025a6a9d28230ab884d903
vcokltfre <vcokltfre@gmail.com>
2020-07-31 12:24:10 +0100

    feat: add user command bypass

--- helpers/checks.py
+++ helpers/checks.py
@@ -3,14 +3,14 @@ Created by vcokltfre - 2020-07-15
 """
 
 from discord.ext import commands
-from salbotlp_secrets.config import MOD_ROLES, BOT_SPAM_CHANNELS, BOT_SPAM_ONLY
+from salbotlp_secrets.config import MOD_ROLES, BOT_SPAM_CHANNELS, BOT_SPAM_ONLY, USER_BYPASS
 
 def botspam():
     async def check(ctx: commands.Context):
         if not BOT_SPAM_ONLY:
             return True
 
-        if any([i.name in MOD_ROLES for i in ctx.author.roles]) or ctx.channel.id in BOT_SPAM_CHANNELS:
+        if any([i.name in MOD_ROLES for i in ctx.author.roles]) or ctx.channel.id in BOT_SPAM_CHANNELS or ctx.author.id in USER_BYPASS:
             return True
 
         raise commands.errors.MissingPermissions(["botspamanywhere"])
