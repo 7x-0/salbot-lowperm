c6db2ae353d5ee14c330f92aaa2625449f6b876b
7x-0 <68181871+7x-0@users.noreply.github.com>
2020-08-09 18:02:50 +0545

    feat: Better implementation of number checking
    Added "not" feature along with any.

--- cogs/convert.py
+++ cogs/convert.py
@@ -18,7 +18,7 @@ class Convert(commands.Cog):
     @botspam()
     async def irgb(self, ctx, i: int):
         """Get RGB values from an integer"""
-        if i > 16777215 or i < 0:
+        if not 0 <= i <= 16777215:
             await ctx.channel.send("The number you entered was invalid. It must be in the range 0 to 16777215", delete_after=15)
             return
         embed=discord.Embed(title=f"{getRGBfromI(i)}", colour=i)
@@ -28,7 +28,7 @@ class Convert(commands.Cog):
     @botspam()
     async def rgbi(self, ctx, r: int, g: int, b: int):
         """Get an integer from RGB values"""
-        if r > 255 or g > 255 or b > 255 or r < 0 or g < 0 or b < 0:
+        if not any(0 <= rrggbb <= 255 for rrggbb in [r,g,b]):
             await ctx.channel.send("One or more of the values you entered was invalid. RGB values must range from 0 to 255", delete_after=15)
             return
         embed=discord.Embed(title=f"{getIfromRGB((r,g,b))}", colour=getIfromRGB((r,g,b)))
